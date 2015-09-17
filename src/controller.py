8612e96489481f3a35b2f4589e97c64851212d8b
Christopher Silvia <cps232@cornell.edu>
2015-09-16 21:32:24 -0400

    Moved enums into config file, added logging messages

--- src/controller.py
+++ src/controller.py
@@ -8,7 +8,7 @@ from config import *
 def respond_to_command(command):
     if command == String("scatter"):
         rospy.loginfo("Received Command: scatter")
-        move_robot(OPEN_GRIPPER,0)
+#        move_robot(OPEN_GRIPPER,0)
     elif command == String("stack_ascending"):
         rospy.loginfo("Received Command: stack_ascending")
     elif command == String("stack_descending"):
@@ -19,19 +19,27 @@ def respond_to_command(command):
 def listener():
     # Initialize node.  This name should be unique, so it is not anonymous
     rospy.init_node('controller')
+    rospy.loginfo("Initialized node Controller")
 
+    rospy.loginfo("Beginning to wait for service /move_robot")
     rospy.wait_for_service("/move_robot")
+    rospy.loginfo("Finished waiting for service /move_robot")
     try:
+        rospy.loginfo("Initializing service proxy for /move_robot...")
         move_robot = rospy.ServiceProxy("/move_robot", MoveRobot)
+        rospy.loginfo("Initialized service proxy for /move_robot")
     
         # the controller subscribes to the /command topic and listens for commands
+        rospy.loginfo("Subscribing to topic /command")
         rospy.Subscriber("command", String, respond_to_command)
+        rospy.loginfo("Subscribed to topic /command")
+
+        rospy.spin()
 
     except rospy.ServiceException, e:
         rospy.logerr("Service Call Failed")
 
 
-    rospy.spin()
 
 
 
--- src/robot_interface.py
+++ src/robot_interface.py
@@ -5,13 +5,7 @@ import rospy
 from std_msgs.msg import String
 from zic_proj1.msg import State
 from zic_proj1.srv import MoveRobot
-
-CLOSE_GRIPPER = 0
-OPEN_GRIPPER = 1
-MOVE_TO_BLOCK = 2
-MOVE_OVER_BLOCK = 3
-MOVE_OVER_TABLE = 4
-
+from config import *
 
 state = State()
 
