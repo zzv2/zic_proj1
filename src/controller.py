2bbf45ee9d6d7d552273b26ef780f533c6259905
dml337 <dml337@cornell.edu>
2015-09-16 20:35:14 -0400

    Added logging for received calls to controller, and added controller to launch file.

--- src/controller.py
+++ src/controller.py
@@ -3,24 +3,27 @@
 import rospy
 from std_msgs.msg import String
 
-def callback(command):
-    pass
+def respond_to_command(command):
+    if command == "scatter":
+        rospy.loginfo("Received Command: scatter")
+    elif command == "stack_ascending":
+        rospy.loginfo("Received Command: stack_ascending")
+    elif command == "stack_descending":
+        rospy.loginfo("Received Command: stack_ascending")
+    else:
+        rospy.logerr("Received Malformed Command")
 
 def listener():
     # Initialize node.  This name should be unique, so it is not anonymous
     rospy.init_node('controller')
 
     # the controller subscribes to the /command topic and listens for commands
-    rospy.Subscriber("command", string, callback)
+    rospy.Subscriber("command", String, respond_to_command)
 
     rospy.spin()
 
 
 
 if __name__ == '__main__':
-    try:
-        talker()
-    except rospy.ROSInterruptException:
-        pass
-
+    listener()
         
