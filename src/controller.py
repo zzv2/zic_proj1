70db28546b7ba9ebb4d8bd4f3aae54dd426f4fbf
Christopher Silvia <cps232@cornell.edu>
2015-09-16 21:48:48 -0400

    Added clearer logging

--- src/controller.py
+++ src/controller.py
@@ -2,7 +2,7 @@
 # license removed for brevity
 import rospy
 from std_msgs.msg import String
-from zic_proj1.srv import MoveRobot
+from zic_proj1.srv import MoveRobot, GetState
 from config import * 
 
 def respond_to_command(command):
@@ -21,19 +21,32 @@ def listener():
     rospy.init_node('controller')
     rospy.loginfo("Initialized node Controller")
 
-    rospy.loginfo("Beginning to wait for service /move_robot")
+    # Initialize the /move_robot service from robot_interface
+    rospy.loginfo("Beginning to wait for service /move_robot...")
     rospy.wait_for_service("/move_robot")
-    rospy.loginfo("Finished waiting for service /move_robot")
+    rospy.loginfo("Service /get_state now available.")
+
+    # Initialize the /get_state service from robot_interface
+    rospy.loginfo("Beginning to wait for service /get_state...")
+    rospy.wait_for_service("/get_state")
+    rospy.loginfo("Service /get_state now available.")
     try:
+        # Initialize the service proxy for the /move_robot server
         rospy.loginfo("Initializing service proxy for /move_robot...")
         move_robot = rospy.ServiceProxy("/move_robot", MoveRobot)
         rospy.loginfo("Initialized service proxy for /move_robot")
+
+        # Initialize the service proxy for the /get_state server
+        rospy.loginfo("Initializing service proxy for /get_state...")
+        get_state = rospy.ServiceProxy("/get_state", GetState)
+        rospy.loginfo("Initialized service proxy for /get_state...")
     
         # the controller subscribes to the /command topic and listens for commands
         rospy.loginfo("Subscribing to topic /command")
         rospy.Subscriber("command", String, respond_to_command)
         rospy.loginfo("Subscribed to topic /command")
 
+        # keep the server open
         rospy.spin()
 
     except rospy.ServiceException, e:
