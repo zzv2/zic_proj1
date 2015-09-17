a0587c6d67cac9f82befa1e8d17ae32dbe852b39
dml337 <dml337@cornell.edu>
2015-09-16 21:11:55 -0400

    Added a config file with enums

--- src/controller.py
+++ src/controller.py
@@ -2,10 +2,13 @@
 # license removed for brevity
 import rospy
 from std_msgs.msg import String
+from zic_proj1.srv import MoveRobot
+from config import * 
 
 def respond_to_command(command):
     if command == String("scatter"):
         rospy.loginfo("Received Command: scatter")
+        move_robot(OPEN_GRIPPER,0)
     elif command == String("stack_ascending"):
         rospy.loginfo("Received Command: stack_ascending")
     elif command == String("stack_descending"):
@@ -17,8 +20,16 @@ def listener():
     # Initialize node.  This name should be unique, so it is not anonymous
     rospy.init_node('controller')
 
-    # the controller subscribes to the /command topic and listens for commands
-    rospy.Subscriber("command", String, respond_to_command)
+    rospy.wait_for_service("/move_robot")
+    try:
+        move_robot = rospy.ServiceProxy("/move_robot", MoveRobot)
+    
+        # the controller subscribes to the /command topic and listens for commands
+        rospy.Subscriber("command", String, respond_to_command)
+
+    except rospy.ServiceException, e:
+        rospy.logerr("Service Call Failed")
+
 
     rospy.spin()
 
